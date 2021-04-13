9fd5463b9d8483cf6cb21d73ccaa618b9e6380ce
Cezar Sa Espinola <cezarsa@gmail.com>
2021-04-12 23:50:53 -0300

    minimum viable mess

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gphotosuploader/googlemirror/api/photoslibrary/v1"
 	"golang.org/x/oauth2"
 )
+
+var errStop = errors.New("stop")
 
 type authConfig struct {
 	ClientID     string   `json:"client_id"`
@@ -99,13 +106,114 @@ func run() error {
 	if err != nil {
 		return err
 	}
-	cli.Albums.List().Pages(ctx, func(response *photoslibrary.ListAlbumsResponse) error {
+
+	allMedia, err := loadCachedAlbum()
+	if err != nil {
+		return err
+	}
+
+	if allMedia == nil {
+		allMedia, err = loadFreshAlbum(ctx, cli)
+		if err != nil {
+			return err
+		}
+	}
+
+	return saveRandPhoto(tc, allMedia)
+}
+
+func saveRandPhoto(cli *http.Client, allMedia []*photoslibrary.MediaItem) error {
+	rand.Seed(time.Now().Unix())
+	mi := randPhoto(allMedia)
+	downloadURL := mi.BaseUrl + "=d"
+	res, err := cli.Get(downloadURL)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid response: %#v", res)
+	}
+	outFile, err := os.Create("img.jpg")
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(outFile, res.Body)
+	if err != nil {
+		return err
+	}
+	return outFile.Close()
+}
+
+func loadFreshAlbum(ctx context.Context, cli *photoslibrary.Service) ([]*photoslibrary.MediaItem, error) {
+	wantedAlbum := os.Getenv("ALBUM")
+	var wantedAlbumID string
+	var albumSize int64
+	err := cli.Albums.List().Pages(ctx, func(response *photoslibrary.ListAlbumsResponse) error {
 		for _, res := range response.Albums {
-			fmt.Printf("album: %s - %s\n", res.Id, res.Title)
+			if wantedAlbum == res.Title {
+				wantedAlbumID = res.Id
+				albumSize = res.TotalMediaItems
+				return errStop
+			}
 		}
 		return nil
 	})
-	return nil
+	if err != nil && err != errStop {
+		return nil, err
+	}
+	if wantedAlbumID == "" {
+		return nil, errors.New("album not found")
+	}
+	fmt.Printf("0/%v\n", albumSize)
+	var allMedia []*photoslibrary.MediaItem
+	err = cli.MediaItems.Search(&photoslibrary.SearchMediaItemsRequest{
+		AlbumId:  wantedAlbumID,
+		PageSize: 100,
+	}).Pages(ctx, func(res *photoslibrary.SearchMediaItemsResponse) error {
+		allMedia = append(allMedia, res.MediaItems...)
+		fmt.Printf("%v/%v\n", len(allMedia), albumSize)
+		return nil
+	})
+	if err != nil && err != errStop {
+		return nil, err
+	}
+	err = saveAlbum(allMedia)
+	return allMedia, err
+}
+
+func saveAlbum(media []*photoslibrary.MediaItem) error {
+	data, err := json.Marshal(media)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(".album.json", data, 0660)
+}
+
+func loadCachedAlbum() ([]*photoslibrary.MediaItem, error) {
+	_, err := os.Stat(".album.json")
+	if os.IsNotExist(err) {
+		return nil, nil
+	}
+	data, err := ioutil.ReadFile(".album.json")
+	if err != nil {
+		return nil, err
+	}
+	var ret []*photoslibrary.MediaItem
+	err = json.Unmarshal(data, &ret)
+	return ret, err
+}
+
+func randPhoto(allMedia []*photoslibrary.MediaItem) *photoslibrary.MediaItem {
+	chosen := rand.Intn(len(allMedia))
+	for {
+		mi := allMedia[chosen]
+		if mi.MediaMetadata == nil || mi.MediaMetadata.Photo == nil {
+			chosen = (chosen + 1) % len(allMedia)
+			continue
+		}
+		return mi
+	}
 }
 
 func main() {
