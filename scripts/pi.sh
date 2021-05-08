c9cb38d903ffa14338f0695c746e1f3259cf5cc5
Cezar Sa Espinola <cezarsa@gmail.com>
2021-05-07 21:42:07 -0300

    adjustments to pi script

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -2,41 +2,71 @@
 
 set -eo pipefail
 
-function init_mass_storage() {
-    if ! [ -f /tmp/piusb.bin ]; then
-        echo "Initializing vfat FS"
-        dd bs=1M if=/dev/zero of=/tmp/piusb.bin count=64
-        mkdosfs /tmp/piusb.bin -F 32 -I
-        mkdir -p /tmp/usb_share
-        sudo mount -ousers,umask=000 /tmp/piusb.bin /tmp/usb_share
-        cp /home/pi/rand-gphotos/placeholders/*.jpg /tmp/usb_share/
-        sync
-        sudo modprobe g_mass_storage file=/tmp/piusb.bin stall=0 ro=1
-    fi
+function download_photos() {
+    dest=$1
+    rm -rf ${dest}
+    mkdir -p ${dest}
 
-    sudo rm -rf /tmp/img-download
-    mkdir -p /tmp/img-download
-
-    export OUTPUT_DIR=/tmp/img-download
+    export OUTPUT_DIR=${dest}
     export CONFIG=/home/pi/gphotos-sync-secret.json
 
-    echo "Updating photos"
+    echo "$(date): Updating photos"
     cd /home/pi/rand-gphotos
     ./rand-gphotos-pi
-
-    echo "Reloading mass storage"
-    sudo modprobe -r g_mass_storage
-    sleep 1
-    cp --no-preserve=mode,ownership /tmp/img-download/*.jpg /tmp/usb_share/
-    sync
-    sudo modprobe g_mass_storage file=/tmp/piusb.bin stall=0 ro=1
-    sudo rm -rf /tmp/img-download
 }
 
+function copy_photos() {
+    img_source=$1
+    img_dest=$2
+    fat_file=$3
+
+    echo "$(date): Creating FS"
+    sudo umount ${img_dest} || true
+    rm -rf ${img_dest}
+    sudo modprobe -r g_mass_storage
+    rm -f ${fat_file}
+    sleep 3
+    mkdir -p $(dirname ${fat_file})
+    dd bs=1M if=/dev/zero of=${fat_file} count=16
+    /usr/sbin/mkdosfs ${fat_file} -F 16 -I
+    mkdir -p ${img_dest}
+    sudo mount -ousers,umask=000 ${fat_file} ${img_dest}
+    cp ${img_source}/*.jpg ${img_dest}/
+    sudo umount ${img_dest}
+    sudo sync
+    load_fs ${fat_file}
+}
+
+function load_fs() {
+    fat_file=$1
+    echo "$(date): Loading mass storage device"
+    sleep 3
+    sudo modprobe g_mass_storage file=${fat_file} stall=0 ro=1 removable=0 # nofua=1 iSerialNumber=1
+}
+
+function init_mass_storage() {
+    img_src="/tmp/img-download"
+    img_dest="/home/pi/mass_storage/usb_share"
+    fat_file="/home/pi/mass_storage/piusb.bin"
+
+    if [ -f ${fat_file} ]; then
+        load_fs ${fat_file}
+    fi
+
+    if [ $1 == "loadonly" ]; then
+        exit 0
+    fi
+
+    download_photos ${img_src}
+    copy_photos ${img_src} ${img_dest} ${fat_file}
+}
+
+sudo touch /var/lock/init_mass_storage
+sudo chmod 666 /var/lock/init_mass_storage
 (
     if ! flock -n 9; then
         echo "lock not available"
         exit 1
     fi
-    init_mass_storage
+    init_mass_storage $1
 ) 9>/var/lock/init_mass_storage
